e0627e192d4793018bd4af1d04733edd646f0999
wingkwong <wingkwong.code@gmail.com>
2018-05-20 01:12:45 +0800

    added parse function draft

--- services/parseService.js
+++ services/parseService.js
@@ -8,6 +8,20 @@ class ParseService {
        this.averageLength = 0;
        this.overallScore = 0;
     }
+
+    parse(data){
+		/*
+			Possible Lines:
+			02/12/2017, 19:18 - Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
+			03/12/2017, 11:47 - Sender: Message
+			04/12/2017, 11:22 - Sender: <Media omitted>
+		*/
+	    for(var l = 0; l < lines.length; l++){
+	      var line = lines[l];
+	      line = splitLine(line);
+
+	    }
+    }
 }
 
 module.exports = new ParseService();
