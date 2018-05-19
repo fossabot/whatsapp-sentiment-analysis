3cad54dc6d572501c34cde942c9c8fab229333b1
wingkwong <wingkwong.code@gmail.com>
2018-05-19 14:18:02 +0800

    added /upload route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 import { Router } from 'express';
-
-const routes = Router();
+var dragDrop = require('drag-drop');
+var fs = require('fs');
+var routes = Router();
 
 /**
  * GET home page
  */
 routes.get('/', (req, res) => {
+
+  res.render('index.html');
+});
+
+/**
+ * POST upload records
+ */
+routes.post('/upload', (req, res) => {
+  fs.readFile(req.files[0].path,'utf8', function(err, data){
+    if (err) throw err;
+  });
   res.render('index.html');
 });
 
