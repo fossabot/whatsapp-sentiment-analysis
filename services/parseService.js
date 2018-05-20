4f1676b84465e63ab72e1f51b0942d38edeb313b
wingkwong <wingkwong.code@gmail.com>
2018-05-21 00:30:41 +0800

    updated regex

--- services/parseService.js
+++ services/parseService.js
@@ -10,16 +10,28 @@ class ParseService {
     }
 
     parse(data){
-		/*
-			Possible Lines:
-			02/12/2017, 19:18 - Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
-			03/12/2017, 11:47 - Sender: Message
-			04/12/2017, 11:22 - Sender: <Media omitted>
-		*/
+    	var lines = data.split('\n');
 	    for(var l = 0; l < lines.length; l++){
 	      var line = lines[l];
 	      line = splitLine(line);
-
+	      var lineLength = line.length;
+	      if(lineLength==1){
+	      	// Possible Line: 
+	      	// Message
+	      }else if(lineLength==3){
+	      	// Possible Line: 
+	      	// 02/12/2017, 19:18 - Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
+	      }else if(lineLength==4){
+	      	// Possible Line: 
+	      	// 03/12/2017, 11:47 - Sender: Message
+	      	// 04/12/2017, 11:22 - Sender: <Media omitted>
+	      	var date = line[1];
+	      	var time = line[2];
+	      	var name = line[3];
+	      	var message = line[4];
+	      }else{
+	      	//unexpected
+	      }
 	    }
     }
 
@@ -27,13 +39,14 @@ class ParseService {
     	/*
     		Input: "03/12/2017, 13:14 - Sender: Message"
 			Return:
-				line[0]: 03/12/2017
-				line[1]: 13:14
-				line[2]: Sender
-				line[3]: Message
+				line[0]: 03/12/2017, 13:14 - Sender: Message
+				line[1]: 03/12/2017
+				line[2]: 13:14
+				line[3]: Sender
+				line[4]: Message
     	*/
-    	var separators = ['\\\,', ' - ', ': '];
-    	return line.split(new RegExp(separators.join('|'),'g'));
+    	var regex = /(.{10}), (.{5}) - (.*?): (.*)/;
+    	return line.match(regex);
     }
 
 }
