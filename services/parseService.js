a8d9270d52a6db8821ec8b1fdde485efece0e776
wingkwong <wingkwong.code@gmail.com>
2018-05-21 23:15:01 +0800

    reworked regex

--- services/parseService.js
+++ services/parseService.js
@@ -1,54 +1,47 @@
 'use strict';
+const DBService = require('./dbService');
 
 class ParseService {
-    constructor() {
-       this.data = [];
-       this.totalMessages = 0;
-       this.totalWords = 0;
-       this.averageLength = 0;
-       this.overallScore = 0;
-    }
-
-    parse(data){
+    process(data){
     	var lines = data.split('\n');
+    	var reg1 = /(.{10}), (.{5}) - (.*?): (.*)/;
+    	var reg2 = /(.{10}), (.{5}) - (.*)/;
+    	var tmp = [];
 	    for(var l = 0; l < lines.length; l++){
-	      var line = lines[l];
-	      line = splitLine(line);
-	      var lineLength = line.length;
-	      if(lineLength==1){
-	      	// Possible Line: 
-	      	// Message
-	      }else if(lineLength==3){
-	      	// Possible Line: 
-	      	// 02/12/2017, 19:18 - Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
-	      }else if(lineLength==4){
-	      	// Possible Line: 
-	      	// 03/12/2017, 11:47 - Sender: Message
-	      	// 04/12/2017, 11:22 - Sender: <Media omitted>
-	      	var date = line[1];
-	      	var time = line[2];
-	      	var name = line[3];
-	      	var message = line[4];
-	      }else{
-	      	//unexpected
-	      }
+	      	var line = lines[l];
+	    	if(reg1.test(line)){
+				/*
+					Input: "03/12/2017, 13:14 - Sender: Message"
+					Return:
+						line[0]: 03/12/2017, 13:14 - Sender: Message
+						line[1]: 03/12/2017
+						line[2]: 13:14
+						line[3]: Sender
+						line[4]: Message
+				*/
+	    		line = line.match(reg1);
+	    		tmp = line;
+	    		var date = line[1], time = line[2],
+		  	 	name = line[3], message = line[4];
+		      	DBService.createRecord(name, date, time, message, 0)
+	    	}else if(reg2.test(line)){
+	    		/*
+					Input: 02/12/2017, 19:18 - Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
+	    		*/
+	    		continue;
+	    	}else{
+	    		/*
+					Input: Lorem
+	    		*/
+	    		var prevLine = tmp;
+	    		var name = prevLine[3];
+	    		var date = prevLine[4];
+	    		var time = prevLine[2];
+	    		var message = line;
+	    		DBService.createRecord(name, date, time, message, 0)
+	    	}
 	    }
     }
-
-    splitLine(line){
-    	/*
-    		Input: "03/12/2017, 13:14 - Sender: Message"
-			Return:
-				line[0]: 03/12/2017, 13:14 - Sender: Message
-				line[1]: 03/12/2017
-				line[2]: 13:14
-				line[3]: Sender
-				line[4]: Message
-    	*/
-    	var regex = /(.{10}), (.{5}) - (.*?): (.*)/;
-    	return line.match(regex);
-    }
-
 }
 
 module.exports = new ParseService();
