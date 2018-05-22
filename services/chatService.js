b09a8b03376a3bfd2499343e0c44e6b8318750ce
wingkwong <wingkwong.code@gmail.com>
2018-05-22 10:30:24 +0800

    renamed and added sentiment score

--- services/chatService.js
+++ services/chatService.js
@@ -1,7 +1,9 @@
 'use strict';
 const DBService = require('./dbService');
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
 
-class ParseService {
+class ChatService {
     process(data){
     	var lines = data.split('\n');
     	var reg1 = /(.{10}), (.{5}) - (.*?): (.*)/;
@@ -23,7 +25,7 @@ class ParseService {
 	    		tmp = line;
 	    		var date = line[1], time = line[2],
 		  	 	name = line[3], message = line[4];
-		      	DBService.createRecord(name, date, time, message, 0)
+		      	DBService.createRecord(name, date, time, message, sentiment.analyze(message).score)
 	    	}else if(reg2.test(line)){
 	    		/*
 					Input: 02/12/2017, 19:18 - Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
@@ -38,10 +40,10 @@ class ParseService {
 	    		var date = prevLine[4];
 	    		var time = prevLine[2];
 	    		var message = line;
-	    		DBService.createRecord(name, date, time, message, 0)
+	    		DBService.createRecord(name, date, time, message, sentiment.analyze(message).score)
 	    	}
 	    }
     }
 }
 
-module.exports = new ParseService();
+module.exports = new ChatService();
