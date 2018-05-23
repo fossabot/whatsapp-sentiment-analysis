a2f7db821eee6f0a0d8ca5ffe75141215f3b7cd4
wingkwong <wingkwong.code@gmail.com>
2018-05-23 23:42:56 +0800

    added result and revised /upload

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,35 @@
 import { Router } from 'express';
-var dragDrop = require('drag-drop');
-var fs = require('fs');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 var routes = Router();
+var ChatService = require('../services/chatService');
 
 /**
  * GET home page
  */
-routes.get('/', (req, res) => {
-
-  res.render('index.html');
-});
+routes.get('/', (req, res) => {	
+  res.render('index', {result: null});
+});	
 
 /**
  * POST upload records
  */
 routes.post('/upload', (req, res) => {
-  fs.readFile(req.files[0].path,'utf8', function(err, data){
-    if (err) throw err;
-  });
-  res.render('index.html');
+  fs.readFileAsync(req.files[0].path, 'utf8').then(function(data){
+  	 ChatService.process(data).then(function(){
+  	 	res.send('File uploaded!');
+  	 });
+  }).catch(function (err) {
+	 	res.status(500).send(err);
+	});
+
 });
+
+routes.get('/result', (req, res) => {
+  	ChatService.getAnalysisResult(function(result){
+  		res.render('index', {result: result});
+  	});
+});	
+
 
 export default routes;
